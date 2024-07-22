3563ea83659ebc68998c3ff8c72fc03099dcbf3a
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-07-22 15:20:35 -0300

    first commit

---
+++ TheLastMini/TheLastMini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheLastMini
+//
+//  Created by Gustavo Horestee Santos Barros on 22/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
