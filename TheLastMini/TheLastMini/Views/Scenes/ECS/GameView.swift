aad9779d3133a04f6fee40bc4276b6968f59c958
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2024-07-31 10:38:11 -0300

    ECS sendo feito

--- TheLastMini/TheLastMini/Views/Scenes/ECS/GameView.swift
+++ TheLastMini/TheLastMini/Views/Scenes/ECS/GameView.swift
@@ -3,12 +3,11 @@ import ARKit
 
 class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, ARSessionDelegate {
     var sceneView: ARSCNView!
-    var vehicleNode: SCNNode!
-    var vehicle: SCNPhysicsVehicle!
     var isVehicleAdded = false
-
-   
-
+    var entities: [Entity] = []
+    let movementSystem = MovementSystem()
+    let renderSystem = RenderSystem()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -16,12 +15,11 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         setupControls()
         setupTapGesture()
     }
-
+    
     func setupTapGesture() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tapGesture)
     }
-
     
     @objc func handleTap(_ gestureRecognizer: UITapGestureRecognizer) {
         let location = gestureRecognizer.location(in: sceneView)
@@ -38,7 +36,7 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         setupVehicle(at: position)
     }
     
-    func setupFloor(){
+    func setupFloor() {
         let floor = SCNFloor()
         let floorNode = SCNNode(geometry: floor)
         floorNode.physicsBody = SCNPhysicsBody(type: .static, shape: SCNPhysicsShape(geometry: floor, options: nil))
@@ -46,8 +44,7 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         floorNode.geometry?.materials.first?.diffuse.contents = UIColor.blue.withAlphaComponent(0.7)
         floorNode.position = SCNVector3(x: 0, y: -1, z: -4)
     }
-
-
+    
     func setupScene() {
         sceneView = ARSCNView(frame: self.view.frame)
         sceneView.delegate = self
@@ -65,7 +62,7 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         
         setupFloor()
     }
-
+    
     func createWheel(lado: Lado) -> SCNNode {
         let wheel = lado == .L ? "Ll.usdz" : "Rr.usdz"
         guard let wheelNode = SCNScene(named: wheel)?.rootNode else {
@@ -73,7 +70,7 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         }
         return wheelNode.clone()
     }
-
+    
     func createChassis() -> SCNNode {
         guard let chassis = SCNScene(named: "MmR.usdz"),
               let chassisNode = chassis.rootNode.childNodes.first else {
@@ -81,13 +78,9 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         }
         return chassisNode
     }
-
+    
     func setupVehicle(at position: SCNVector3) {
-       
-        
         let chassisNode = createChassis()
-      
-        
         let body = SCNPhysicsBody(type: .dynamic, shape: nil)
         body.mass = 1.0
         chassisNode.physicsBody = body
@@ -110,7 +103,7 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         wheel2.connectionPosition = SCNVector3(x, y, z)
         wheel3.connectionPosition = SCNVector3(-x, y, -z)
         wheel4.connectionPosition = SCNVector3(x, y, -z)
-
+        
         wheel1.suspensionStiffness = CGFloat(1)
         wheel2.suspensionStiffness = CGFloat(1)
         wheel3.suspensionStiffness = CGFloat(1)
@@ -129,33 +122,19 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         SCNTransaction.begin()
         SCNTransaction.completionBlock = {
             self.sceneView.scene.physicsWorld.addBehavior(vehicle)
-            self.vehicle = vehicle
-            self.vehicleNode = chassisNode
+            self.entities.append(chassisNode)
+            
+            let vehicleComponent = VehiclePhysicsComponent(vehicle: vehicle, wheels: [wheel1, wheel2, wheel3, wheel4])
+            let positionComponent = PositionComponent(position: position)
+            
+            chassisNode.addComponent(vehicleComponent)
+            chassisNode.addComponent(positionComponent)
         }
         SCNTransaction.commit()
         
         isVehicleAdded = true
     }
-
-
-    func createPlane(anchor: ARPlaneAnchor) -> SCNNode {
-        let planeNode = SCNNode()
-        let geometry = SCNPlane(width: CGFloat(anchor.planeExtent.width), height: CGFloat(anchor.planeExtent.height))
-        let material = SCNMaterial()
-        material.diffuse.contents = UIColor.transparentLightBlue
-        geometry.materials = [material]
-        
-        planeNode.geometry = geometry
-        planeNode.position = SCNVector3(anchor.center.x, anchor.center.y, anchor.center.z)
-        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
-        
-        let physicsShape = SCNPhysicsShape(geometry: geometry, options: nil)
-        let physicsBody = SCNPhysicsBody(type: .static, shape: physicsShape)
-        planeNode.physicsBody = physicsBody
-        
-        return planeNode
-    }
-
+    
     func setupControls() {
         let leftButton = UIButton(frame: CGRect(x: 20, y: self.view.frame.height - 120, width: 100, height: 50))
         leftButton.backgroundColor = .green
@@ -185,99 +164,52 @@ class GameView: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate, 
         backwardButton.addTarget(self, action: #selector(resetSpeed), for: .touchUpInside)
         self.view.addSubview(backwardButton)
     }
-
+    
     @objc func moveForward() {
-        let force: CGFloat = 1 // Aumentar a força aplicada
-        vehicle.applyEngineForce(force, forWheelAt: 2) // Traseiras
-        vehicle.applyEngineForce(force, forWheelAt: 3)
+        movementSystem.engineForce = 1
     }
-
+    
     @objc func moveBackward() {
-        let force: CGFloat = -1 // Aumentar a força aplicada
-        vehicle.applyEngineForce(force, forWheelAt: 2) // Traseiras
-        vehicle.applyEngineForce(force, forWheelAt: 3)
+        movementSystem.engineForce = -1
     }
-
+    
     @objc func turnRight() {
-        vehicle.setSteeringAngle(-0.5, forWheelAt: 0) // Dianteiras
-        vehicle.setSteeringAngle(-0.5, forWheelAt: 1)
+        movementSystem.steeringAngle = -0.5
     }
-
+    
     @objc func turnLeft() {
-        vehicle.setSteeringAngle(0.5, forWheelAt: 0) // Dianteiras
-        vehicle.setSteeringAngle(0.5, forWheelAt: 1)
+        movementSystem.steeringAngle = 0.5
     }
-
-    @objc func resetOrientation(){
-        vehicle.setSteeringAngle(0.0, forWheelAt: 0)
-        vehicle.setSteeringAngle(0.0, forWheelAt: 1)
+    
+    @objc func resetOrientation() {
+        movementSystem.steeringAngle = 0.0
     }
-
-    @objc func resetSpeed(){
-        let force: CGFloat = 0 // Aumentar a força aplicada
-        vehicle.applyEngineForce(force, forWheelAt: 2) // Traseiras
-        vehicle.applyEngineForce(force, forWheelAt: 3)
+    
+    @objc func resetSpeed() {
+        movementSystem.engineForce = 0
     }
-//
-//    var lastUpdateTime: TimeInterval = 0
-//
-//    // MARK: - ARSCNViewDelegate
-//    
-//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-//        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
-//        
-//        let plane = createPlane(anchor: planeAnchor)
-//        
-//        node.addChildNode(plane)
-//    }
-//
-//    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-//        guard let planeAnchor = anchor as? ARPlaneAnchor,
-//              let planeNode = node.childNodes.first,
-//              let plane = planeNode.geometry as? SCNPlane else { return }
-//        
-//        plane.width = CGFloat(planeAnchor.planeExtent.width)
-//        plane.height = CGFloat(planeAnchor.planeExtent.height)
-//        planeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
-//    }
-//
-//    func sessionWasInterrupted(_ session: ARSession) {
-//        // Inform the user that the session has been interrupted, for example, by presenting an overlay.
-//    }
-//
-//    func sessionInterruptionEnded(_ session: ARSession) {
-//        resetTracking()
-//    }
-//
-//    func session(_ session: ARSession, didFailWithError error: Error) {
-//        guard error is ARError else { return }
-//        
-//        let errorWithInfo = error as NSError
-//        let messages = [
-//            errorWithInfo.localizedDescription,
-//            errorWithInfo.localizedFailureReason,
-//            errorWithInfo.localizedRecoverySuggestion
-//        ]
-//        
-//        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
-//        
-//        DispatchQueue.main.async {
-//            let alertController = UIAlertController(title: "The AR session failed.", message: errorMessage, preferredStyle: .alert)
-//            let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
-//                alertController.dismiss(animated: true, completion: nil)
-//                self.resetTracking()
-//            }
-//            alertController.addAction(restartAction)
-//            self.present(alertController, animated: true, completion: nil)
-//        }
-//    }
-//
-//    private func resetTracking() {
-//        let configuration = ARWorldTrackingConfiguration()
-//        configuration.planeDetection = [.horizontal, .vertical]
-//        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
-//    }
+    
+    // Atualizar a lógica de jogo a cada frame
+    func update(deltaTime: TimeInterval) {
+        movementSystem.update(deltaTime: deltaTime, entities: entities)
+        renderSystem.update(deltaTime: deltaTime, entities: entities)
+//        movementSystem.resetCommands()
+    }
+    
+    // Chamar a função de atualização de jogo no loop principal
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let displayLink = CADisplayLink(target: self, selector: #selector(gameLoop))
+        displayLink.add(to: .current, forMode: .default)
+    }
+    
+    @objc func gameLoop(displayLink: CADisplayLink) {
+        let deltaTime = displayLink.duration
+        update(deltaTime: deltaTime)
+    }
 }
+
 
 enum Lado{
     case L
---
+++ TheLastMini/TheLastMini/Views/Scenes/ECS/System.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  TheLastMini
+//
+//  Created by Jairo Júnior on 31/07/24.
+//
+
+import Foundation
+import RealityKit
+
+class RenderSystem {
+    func update(deltaTime: TimeInterval, entities: [Entity]) {
+        for entity in entities {
+            guard let position = entity.getComponent(ofType: PositionComponent.self) else { continue }
+            
+            // Atualizar a posição do node
+//            entity.position = position.position
+//            print(position)
+//            print("entity.position", entity.position)
+        }
+    }
+}
+
+class MovementSystem {
+    var steeringAngle: CGFloat = 0.0
+    var engineForce: CGFloat = 0.0
+    
+    func update(deltaTime: TimeInterval, entities: [Entity]) {
+        for entity in entities {
+            guard let vehiclePhysics = entity.getComponent(ofType: VehiclePhysicsComponent.self) else { continue }
+            
+            vehiclePhysics.vehicle.setSteeringAngle(steeringAngle, forWheelAt: 0)
+            vehiclePhysics.vehicle.setSteeringAngle(steeringAngle, forWheelAt: 1)
+            vehiclePhysics.vehicle.applyEngineForce(engineForce, forWheelAt: 2)
+            vehiclePhysics.vehicle.applyEngineForce(engineForce, forWheelAt: 3)
+        }
+    }
+}
+
+
+
